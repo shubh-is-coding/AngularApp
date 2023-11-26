6b5d45fbaeed0ed00fd205d48f4ce9a9e10f7531
shubh-is-coding <heythereshubh@gmail.com>
2023-11-26 18:46:43 +0530

    navbar changes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,27 +1,49 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import VanillaTilt from 'vanilla-tilt';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-
-
-export class MenuComponent {
- 
+export class MenuComponent implements OnInit {
   ngOnInit() {
-
     VanillaTilt.init(
-      this.el.nativeElement.querySelectorAll(".list-item1, .list-item2, .list-item3"), { max: 30, speed: 500, scale: 1.3 }
-    );}
+      this.el.nativeElement.querySelectorAll(
+        '#list-item1, #list-item2, #list-item3'
+      ),
+      { max: 50, speed: 500, scale: 1.4 }
+    );
 
-    constructor(private el: ElementRef) {
-    }
-    
-  items: String[] = ['My Skills', 'Work', 'Contact'];
-  vanillaTilt: any;  
+    const icon1: any = document.getElementById('list-item1');
+    const icon2: any = document.getElementById('list-item2');
+    const icon3: any = document.getElementById('list-item3');
+
+    gsap.from(icon1, {
+      x: -200,
+      duration: 0.6,
+      delay: 2.8,
+      opacity: 0,
+    });
+    gsap.from(icon2, {
+      x: -200,
+      duration: 0.6,
+      delay: 2.6,
+      opacity: 0,
+    });
+    gsap.from(icon3, {
+      x: -200,
+      duration: 0.6,
+      delay: 2.4,
+      opacity: 0,
+    });
+  }
+
+  constructor(private el: ElementRef) {}
+
+  vanillaTilt: any;
 }
